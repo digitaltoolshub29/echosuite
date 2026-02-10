004f4c4a5e28773c09752fabdc04725008f7c356
digitaltoolshub29 <fatmamsh28529@gmail.com>
2026-02-10 13:51:28 +0000

    feat: connect to db and create user model

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express();
 
@@ -7,6 +9,11 @@ const PORT = 3000;
 app.get('/', (req, res) => {
   res.send('The server is alive!');
 });
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
