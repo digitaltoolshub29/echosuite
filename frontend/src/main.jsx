03da7816702f83697a96e67689bb829732eba9a3
digitaltoolshub29 <fatmamsh28529@gmail.com>
2026-02-11 07:20:34 +0000

    feat: create login and register pages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,13 +6,24 @@ import {
 } from 'react-router-dom';
 import App from './App';
 import './index.css';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
   },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/register',
+    element: <Register />,
+  },
 ]);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
