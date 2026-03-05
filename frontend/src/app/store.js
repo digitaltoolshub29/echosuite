935c54e9eadd865341ab347c45a564e460ab567e
digitaltoolshub29 <fatmamsh28529@gmail.com>
2026-03-05 03:50:49 +0000

    Revert: Simplify build config and correct paths

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,6 +1,6 @@
-import authReducer from '@/features/auth/authSlice'
-import goalReducer from '@/features/goals/goalSlice'
-
+import { configureStore } from '@reduxjs/toolkit'
+import authReducer from '../features/auth/authSlice'
+import goalReducer from '../features/goals/goalSlice'
 
 export const store = configureStore({
   reducer: {
